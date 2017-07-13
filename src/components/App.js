9846fe6d97640e8969d49038fcf100016e88b977
Anna Fuentes Jakobsen <annafjakobsen@gmail.com>
2017-07-13 13:33:53 +0200

    added components to repo

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Button from './Button';
+import logo from './../logo.svg';
+import './../App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Button />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Button.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Button extends Component {
+  render(){
+    return(
+      <button>Add Task</button>
+    );
+  }
+}
+
+export default Button;
